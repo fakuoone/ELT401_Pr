c2306ea26d11c2f12e2e5b07ac9f237306846c2d
Justus TOWER <justus@darmsign.al>
2019-12-01 18:16:50 +0100

    W5 fertig gestellt

--- W05/Lab5_3.cpp
+++ W05/Lab5_3.cpp
@@ -1,189 +1,241 @@
-/*PragrammMathepruefungDatum:19.11.2019Max MustermannPraktikum ELT401Seite: 5
-JustusWeinholdVersion1.0*/
-#include <stdio.h> 
-#include <conio.h> 
-//fuergetch
-#include<Windows.h>
-//fuersystem
-#define ESC27    //Tasten code fuer die Taste "ESC" um Programm zu beenden
-//Variablen
-int zahl1, zahl2; 
-int k_addition, k_subtraktion, k_multiplikation, k_division, k_rest; 
-int ergebnis_addition, ergebnis_subtraktion, ergebnis_multiplikation, ergebnis_division, ergebnis_rest; 
-int scanf_return; 
-float AuswertungGesamt; 
-float richtig=0; 
-char programm_ende=0; 
-//Funktionsdeklaration
-int summe(int x, int y); 
-int differenz(int x, int y);  
-int multiplikation(int x, int y); 
-int division(int x, int y); 
-int rest(int x, int y); 
+/*************************************************************************
+	Dateiname:			Lab5_3_2.cpp
+	Beschreibung: 		Mathepruefung
+	Version:			1.0
+	Aenderungsgrund: 	-
+	Projekt:    		Mathepruefung 
+	Datum:      		28.11.2019
+	Bearbeiter:			
+*************************************************************************/
 
-int erste_zahl(void){
+/* Einbinden Bibliotheken */
+#include <stdio.h>
 
+/* Definition von Makros */
+#define ENTER 10
+#define ESC 27
+
+    /* Definition globaler Variablen */
+    int user_ergebnisse[4];
+
+/* Funktion zur Berechnung der Summe */
+int addition(int x, int y){
+    return (x + y);
 }
 
+/* Funktion zur Berechnung der Subtraktion */
+int subtraktion(int x, int y){
+    return (x - y);
+}
 
-//Hauptprogramm
-int main(void){
-    do{ 
-    //Kopfzeile
-        printf("Rechenprogramm fuer die 4 Grundrechenarten\n");
-        printf("------------------------------------------\n"); 
-        //Abfragen der ersten Zahl mit hinweis auf Fehleingabe
-        do{
-            //1.Schleife zur Kontrolle der richtigen Eingabe fuer Zahl 1 
-            printf("Geben Sie die 1. Zahl hier ein(1-100):\t");
-            scanf_return=scanf_s("%5d", &zahl1); 
-            //fehlerhafte Eingabe = Kontrolle 
-            if(0 == scanf_return || 0 > zahl1 || 100 < zahl1) {
-                printf("Sie haben leider keine gueltige ganze Zahl eingegeben! Bitte geben Sie eine richtige Zahl ein.\n"); 
-                //(Tastaturpuffer) Zeichenweise Tastatur leeren bis Enter
-                while (getchar() != '\n');
-            }
-        } while(0 == scanf_return || 0 > zahl1 || 100 < zahl1); 
-        //Abfragen der zweiten Zahl mit hinweis auf Fehleingabe
-        do{
-            //2.Schleife zur Kontrolle der richtigen Eingabe fuer Zahl 2 
-            printf("Geben Sie die 2. Zahl hier ein(1-100):\t"); scanf_return=scanf_s("%5d", &zahl2); 
-            //fehlerhafte Eingabe = Kontrolle 
-            if(0 == scanf_return || 0 > zahl2 || 100 < zahl2){
-                printf("Sie haben leider keine gueltige ganze Zahl eingegeben! Bitte geben Sie eine richtige Zahl ein.\n"); 
-                while(getchar()!='\n'); //(Tastaturpuffer) Zeichenweise Tastatur leeren bis Enter 
-            } 
-        } while(0 == scanf_return || 0 > zahl2 || 100 < zahl2);
-         
-        //Berechnung der Additions aufgabe 
-        k_addition = summe(zahl1, zahl2); 
-        //Berechnung der Subtraktion 
-        k_subtraktion = differenz(zahl1, zahl2); 
-        //Berechnung der Multiplikation 
-        k_multiplikation = multiplikation(zahl1, zahl2); 
-        //Berechnung Division 
-        k_division = division(zahl1, zahl2); 
-        //Berechnung Rest 
-        k_rest = rest(zahl1, zahl2);
-        //Abfrage der Rechenergebnisse
-        //Schleife fuer hinweis auf falsch eingebene Zahl
-        do{
-            printf("%d + %d= ", zahl1, zahl2); 
-            scanf_return = scanf_s("%d", &ergebnis_addition); 
-            //fehlerhafte Eingabe = Kontrolle 
-            if(0 == scanf_return){
-                printf("Sie haben leider keine gueltige ganze Zahl eingegeben! Bitte geben Sie eine richtige Zahlein.\n");
-                while(getchar() != '\n'); 
-                //(Tastaturpuffer) Zeichenweise Tastatur leeren bis Enter
-            }
-        } while(0 == scanf_return || 0 > zahl2 || 100 < zahl2); 
-        //Schleife fuer hinweis auf falsch eingebene Zahl
-        do {
-            printf("%d - %d = ", zahl1, zahl2); 
-            scanf_return = scanf_s("%d", &ergebnis_subtraktion); 
-            //fehlerhafte Eingabe = Kontrolle 
-            if(0 == scanf_return) {
-                printf(" Sie haben leider keine gueltige ganze Zahl eingegeben! Bitte geben Sie eine richtige Zahl ein.\n"); 
-                while(getchar() != '\n'); //(Tastaturpuffer) Zeichenweise Tastatur leeren bis Enter
-            }
-        } while(0 == scanf_return || 0 > zahl2 || 100 < zahl2); 
-        //Schleife fuer hinweis auf falsch eingebene Zahl
-        printf("%d * %d = ", zahl1, zahl2); 
-        scanf_return = scanf_s("%d", &ergebnis_multiplikation); 
-        //fehlerhafte Eingabe = Kontrolle 
-        if( 0 == scanf_return) {
-            printf("Sie haben leider keine gueltige ganze Zahl eingegeben! Bitte geben Sie eine richtige Zahl ein.\n");
-        do{
-            while(getchar() != '\n');  //(Tastaturpuffer) Zeichenweise Tastatur leeren bis Enter
-        }
-    } while(0 == scanf_return || 0 > zahl2 || 100 < zahl2); 
-    //Schleife fuer hinweis auf falsch eingebene Zahl 
-    printf("%d / %d = ", zahl1, zahl2); 
-    scanf_return = scanf_s("%d", &ergebnis_division); //fehlerhafte Eingabe = Kontrolle 
-    if( 0 == scanf_return){
-        printf("Sie haben leider keine gueltige ganze Zahl eingegeben! Bitte geben Sie eine richtige Zahl ein.\n");
-        while (getchar() != '\n'); //(Tastaturpuffer) Zeichenweise Tastatur leeren bis Enter
-    }
-} while( 0 == scanf_return || 0 > zahl2 || 100 < zahl2); 
-//Schleife fuer hinweis auf falsch eingebene Zahl 
-printf("Der Rest von %d / %d ist: ", zahl1, zahl2); 
-scanf_return = scanf_s("%d", &ergebnis_rest); 
-//fehlerhafte Eingabe = Kontrolle
-if(0 == scanf_return) {
-    printf("Sie haben leider keine gueltige ganze Zahl eingegeben! Bitte geben Sie eine richtige Zahl ein.\n"); 
-    while(getchar() != '\n'); //(Tastaturpuffer) Zeichenweise Tastatur leeren bis Enter
+/* Funktion zur Berechnung der Multiplikation */
+int multiplikation(int x, int y){
+    return (x * y);
 }
-} while( 0 == scanf_return || 0 > zahl2 || 100 < zahl2); 
-//AbgleichderErgebnisse
-//Abgleich Addition
-if(ergebnis_addition == k_addition) { 
-    printf("\nIhr selbst errechnetes Ergebnis der Addition %d ist richtig.\n", ergebnis_addition);
-    richtig++;
-} else {
-    do {
-        do {
-            printf("\nIhr selbst errechnetes Ergebnis der Addition%distflasch!Richtig waere %d.\n", ergebnis_addition, k_addition); 
-        } 
-        //Abgleich Subtraktion
-        if(ergebnis_subtraktion == k_subtraktion) {
-            printf("\nIhr selbst errechnetes Ergebnis der Subtraktion %d ist richtig.\n", ergebnis_subtraktion); 
-            richtig++; 
-        } else { 
-            printf("\nIhre selbst errechnetes Ergebnis der Subtraktion %d ist flasch! Richtig waere %d.\n", ergebnis_subtraktion, k_subtraktion); 
-        }
-        //Abgleich Multiplikation
-        if(ergebnis_multiplikation == k_multiplikation) {
-            printf("\nIhr selbst errechnetes Ergebnis der Multiplikation %d ist richtig.\n", ergebnis_multiplikation); 
-            richtig++;
-        } else {
-            printf("\nIhr selbst errechnetes Ergebnis der Multiplikation %d ist flasch! Richtig waere%d.\n", ergebnis_multiplikation, k_multiplikation);
-        }
-        //Abgleich Division 
-        if(ergebnis_division == k_division) {
-            printf("\nIhr selbst errechnetes Ergebnis der Division %d ist richtig.\n", ergebnis_division); 
-            richtig++; 
-            } else { 
-                printf("\nIhr selbst errechnetes Ergebnis der Division%distflasch!Richtigwaere%d.\n", ergebnis_division, k_division); }
-        //AbgleichRest
-        if(ergebnis_rest==k_rest) {
-            printf("\nIhr selbst errechnetes Ergebnis fuer den Rest %d ist richtig.\n",  ergebnis_rest); 
-            richtig++; 
-        }
-        else { 
-            printf("\nIhr selbst errechnetes Ergebnis fuer den Rest %d ist flasch! Richtig waere %d.\n",  ergebnis_rest,  k_rest);  
-        } 
-        //Auswertung in Prozent der selbst ausgerechneten Ergebnisse
-        AuswertungGesamt = richtig/5*100; 
-        richtig = 0; 
-        printf("\nSie haben %.0f prozent richtig erechnete Ergebnisse.\n\n", AuswertungGesamt); 
-        //Abfrage zum Programm beenden
-        printf("Programm beenden mit 'ESC', zum Fortsetzen beliebige andere Taste druecken.\n"); 
-        //Tastaturabfrage 1 zeichen
-        programm_ende = _getch(); 
-        //Bildschirm loeschen
-        system("cls");
-        } while(programm_ende != ESC); 
-        //Code, das Programm so lange ausgefuehrt wird, bis ESC gedrueckt wird
-        return0; 
-}
-//Rueckgabe der Werte
-//Rueckgabe der Addition
-int summe(int x, int y) {
-    return(x+y); 
-}
-//Rueckgabe der Subtraktion
-int differenz(int x, int y) {
-    return(x-y); 
-}
-//Rueckgabe der Multiplikation
-int multiplikation(int x, int y) {
-    return(x*y);
-} 
-//Rueckgabe der Division
+
+/* Funktion zur Berechnung der Division */
 int division(int x, int y) {
-    return(x/y); 
+    return (x / y);
 }
-//Rueckgabe Rest 
-int rest(int x, int y) {
-    return(x%y); 
+
+/* Funktion zur Berechnung des Modulus */
+int modulus(int x, int y) {
+    return (x % y);
+}
+
+int abfrage_zahl1(void) {
+    /* Steuervariablen fuer Eingabefehler */
+    int scanf_return;
+    char enter;
+    int operand;
+    do {
+        printf("| Bitte eine Zahl zwischen 1 und 100 eingeben: ");
+        scanf_return = scanf("%d%c", &operand, &enter);
+        if ((scanf_return == 0) || (enter != ENTER)) {
+            printf("| Sie haben keine gueltige Zahl eingegeben!\n| Bitte geben Sie eine richtige Zahl ein.\n");
+            while (getchar() != ENTER);
+        } else if (enter != ENTER) {
+                while (getchar() != ENTER);
+                scanf_return = 0;
+        } else if ((operand < 1) || (operand > 100)) {
+            printf("| Die Zahl ist ausserhalb des Bereichs!\n");
+            scanf_return = 0;
+        }
+    } while(scanf_return == 0);
+    return operand;
+}
+
+int abfrage_zahl2(void) {
+    /* Steuervariablen fuer Eingabefehler */
+    int scanf_return;
+    char enter;
+    
+    int operand;
+    do {
+        printf("| Bitte eine weitere Zahl zwischen 1 und 100 eingeben: ");
+        scanf_return = scanf("%d%c", &operand, &enter);
+        if ((scanf_return == 0) || (enter != ENTER)) {
+            printf("| Sie haben keine gueltige Zahl eingegeben!\n| Bitte geben Sie eine richtige Zahl ein.\n");
+            while (getchar() != ENTER);
+        } else if (enter != ENTER) {
+                while (getchar() != ENTER);
+                scanf_return = 0;
+        } else if ((operand < 1) || (operand > 100)) {
+            printf("| Die eingegebene Zahl ist ausserhalb des vorgebenen Bereichs!\n");
+            scanf_return = 0;
+        }
+    } while(scanf_return == 0);
+    return operand;
+}
+
+int abfrage_ergebnisse(void) {
+    /* Steuervariablen fuer Eingabefehler */
+    int scanf_return;
+    char enter;
+
+    for (size_t i = 0; i < 5; i++) {
+        do {
+            switch (i) {
+            case 0:
+                printf("| Bitte die erwartete Summe eingeben:       ");
+                break;
+            case 1:
+                printf("| Bitte die erwartete Differenz eingeben:   ");
+                break;
+            case 2:
+                printf("| Bitte das erwartetes Produkt eingeben:    ");
+                break;
+            case 3:
+                printf("| Bitte den erwarteten Quotienten eingeben: ");
+                break;
+            case 4:
+                printf("| Bitte den erwarteten Rest eingeben:       ");
+                break;
+            }
+            scanf_return = scanf("%d%c", &user_ergebnisse[i], &enter);
+            if ((scanf_return == 0)  || (enter != ENTER))  {
+                printf("| Sie haben keine gueltige Zahl eingegeben!\n| Bitte geben Sie eine richtige Zahl ein.\n");
+                while (getchar() != ENTER);
+            }
+            else if (enter != ENTER) {
+                while (getchar() != ENTER);
+                scanf_return = 0;
+            }
+        } while (scanf_return == 0);
+    }
+    return 0;
+}
+
+int main(void){
+    /* Eingabevariablen */
+    int zahl1, zahl2;
+    /* Ausgabevariablen */
+    int ergebnisse[4];
+    int gesamt_richtig = 0, 
+        durchlauf = 0; 
+    float prozent;
+
+    printf("+---------------------------------------------------------------+\n");
+    printf("|      ###   ###    ####   ########  ##   ##  ######            |\n");
+    printf("|      #### ####   ##  ##     ##     ##   ##  ##                |\n");
+    printf("|      ## ### ##  ########    ##     #######  ####    ####      |\n");
+    printf("|      ##     ##  ##    ##    ##     ##   ##  ##                |\n");
+    printf("|      ##     ##  ##    ##    ##     ##   ##  ######            |\n");
+    printf("|                                                               |\n");
+    printf("| ######   ######   ##   ##  #######  ##   ##  ##   ##   #####  |\n");
+    printf("| ##   ##  ##   ##           ##       ##   ##  ###  ##  ##      |\n");
+    printf("| ######   ######   ##   ##  ######   ##   ##  ## # ##  ##  ### |\n");
+    printf("| ##       ##   ##  ##   ##  ##       ##   ##  ##  ###  ##   ## |\n");
+    printf("| ##       ##   ##    ###    ##         ###    ##   ##   ###### |\n");
+    printf("|                                                            ## |\n");
+    printf("+---------------------------------------------------------------+\n");
+
+    do {
+        /* Variablen fur einen Durchlauf */
+        int richtig = 0;
+        int gesamt_fragen;
+
+        /* Durchlauf initialisieren */
+        durchlauf++;
+        gesamt_fragen = 5 * durchlauf;
+
+        printf("+---------------------------------------------------------------+\n");
+        printf("|                           RUNDE %2d                            |\n", durchlauf);
+        printf("+---------------------------------------------------------------+\n");
+
+        /* User-Eingaben */
+        zahl1 = abfrage_zahl1();
+        zahl2 = abfrage_zahl2();
+                printf("+---------------------------------------------------------------+\n");
+
+        abfrage_ergebnisse();
+        /* Berechnung */
+        ergebnisse[0] = addition(zahl1, zahl2);
+        ergebnisse[1] = subtraktion(zahl1, zahl2);
+        ergebnisse[2] = multiplikation(zahl1, zahl2);
+        ergebnisse[3] = division(zahl1, zahl2);
+        ergebnisse[4] = modulus(zahl1, zahl2);
+
+        /* Auswertung: Rechnen */
+                printf("+---------------------------------------------------------------+\n");
+
+        for (int i = 0; i < 5; i++) {
+            printf("| ");
+            switch (i) {
+            case 0:
+                printf("Summe"); 
+                break;
+            case 1:
+                printf("Differenz"); 
+                break;
+            case 2:
+                printf("Produkt");
+                break;
+            case 3:
+                printf("Quotient");
+                break;
+            case 4:
+                printf("Rest");
+                break;
+            default:
+                break;
+            }
+
+            if (ergebnisse[i] == user_ergebnisse[i]){
+                richtig++;
+                printf(" korrekt!\n");
+            } else {
+                printf(" falsch. Die richtige Antwort waere %d\n", ergebnisse[i]);
+            }
+        }
+
+        /* Auswertung: Durchlauf */
+                printf("+---------------------------------------------------------------+\n");
+
+        if (richtig == 1) {
+            printf("| In dieser Runde wurde %d von 5 Fragen richtig beantwortet. ", richtig);
+        } else {
+            printf("| In dieser Runde wurden %d von 5 Fragen richtig beantwortet. ", richtig);
+        }
+        prozent = 100 * richtig / 5;
+        printf("\n| Das entspricht %.2f%%.\n", prozent);
+
+            /* Auswertung: Gesamt */
+            gesamt_richtig += richtig;
+        if (richtig == 1) {
+            printf("|\n| Im der %d. Runde wurde insgesamt %d von %d Fragen richtig beantwortet.\n", durchlauf, gesamt_richtig, gesamt_fragen);
+        } else {
+            printf("|\n| Im der %d. Runde wurden insgesamt %d von %d Fragen richtig beantwortet.\n", durchlauf, gesamt_richtig, gesamt_fragen);
+        }
+        prozent = 100 * gesamt_richtig / gesamt_fragen;
+        printf("| Das entspricht %.2f%%.\n", prozent);
+
+        /* Abfrage fuer weitere Runde */
+        printf("+---------------------------------------------------------------+\n");
+
+        printf("| Programm beenden mit 'ESC'.\n| Zum Fortsetzen beliebige andere Taste\n");
+    } while (getchar() != ESC);
+    printf("+---------------------------------------------------------------+\n");
+
+    return 0;
 }

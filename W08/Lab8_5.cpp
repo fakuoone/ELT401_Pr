474910061cff85af2af4e38ecc047cb1674d7d83
JustusDinera <justus.weinhold@zoho.com>
2019-12-05 14:23:06 +0100

    Update Lab8_5.cpp

--- W08/Lab8_5.cpp
+++ W08/Lab8_5.cpp
@@ -1,11 +1,11 @@
 /*************************************************************************
-	Dateiname:			Lab8_5.cpp
-	Beschreibung: 		Ausgabe einer 9x9 Matrix mit dem kleinen "Ein-Mal-Eins"
-	Version:			1.1
-	Aenderungsgrund: 	-
-	Projekt:    		"Eim-Mal-Eins"-Matrix"
-	Datum:      		28.11.2019
-	Bearbeiter:			
+Dateiname:			Lab8_5.cpp
+Beschreibung: 		Ausgabe einer 9x9 Matrix mit dem kleinen "Ein-Mal-Eins"
+Version:			1.1
+Aenderungsgrund: 	-
+Projekt:    		"Eim-Mal-Eins"-Matrix"
+Datum:      		28.11.2019
+Bearbeiter:
 *************************************************************************/
 
 /* Einbinden der Bibliotheken */
@@ -15,14 +15,16 @@
 #define NIEDRIGSTER_ZAHL 1
 #define HOECHSTE_ZAHL 9
 
-int main(void){
-    for (int i = NIEDRIGSTER_ZAHL; i <= HOECHSTE_ZAHL; i++)
-    {
-        for (int j = NIEDRIGSTER_ZAHL; j <= HOECHSTE_ZAHL; j++)
-        {
-            printf("%dx%d=%2d  ",i,j,(i*j));
-        }
-    printf("\n");
-    }
-    return 0;
+int main(void) {
+	/* Zaehlvariablen */
+	int i, j;
+	for (i = NIEDRIGSTER_ZAHL; i <= HOECHSTE_ZAHL; i++)
+	{
+		for (j = NIEDRIGSTER_ZAHL; j <= HOECHSTE_ZAHL; j++)
+		{
+			printf("%dx%d=%2d  ", i, j, (i*j));
+		}
+		printf("\n");
+	}
+	return 0;
 }
